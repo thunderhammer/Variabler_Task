dd28d7abe47f7265eb0a3696becaabefe93a27b8
Morten Engemand <engemand@hotmail.com>
2018-05-14 15:58:53 +0200

    Checkin

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -6,10 +6,66 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp2
 {
-    class Program
+    enum filtyper
     {
-        static void Main(string[] args)
+        csv,
+        pdf,
+        txt
+    }
+
+    class Program
+    {        enum filtyper
         {
+            csv,
+            pdf,
+            txt
+        };
+
+        struct Person
+        {
+            public int Id;
+            public string Navn;
+        };
+
+        static void Main(string[] args)
+        {               
+            int heltal = 10;
+            heltal++;
+            heltal--;
+            heltal += 20;
+            Console.WriteLine(heltal);
+
+            double kommatal = 12.5;
+            kommatal++;
+            kommatal--;
+            kommatal *= 2;
+            Console.WriteLine(kommatal);
+
+            filtyper ft = filtyper.csv;
+            Console.WriteLine(ft);
+            Console.WriteLine(Convert.ToInt32(ft));
+
+            DateTime dato = DateTime.Now;
+            Console.WriteLine(dato.ToString("ddMMyy"));
+
+            Person p = new Person();
+            p.Id = 1;
+            p.Navn = "Mikkel";
+            Console.WriteLine(p.Navn);
+
+
+
+            if (System.Diagnostics.Debugger.IsAttached)
+            {
+                Console.WriteLine("Tryk på en tast...");
+                Console.ReadKey();
+            }
+
+
+
+
+
         }
+
     }
 }
